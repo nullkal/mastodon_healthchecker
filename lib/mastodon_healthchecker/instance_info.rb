b4583bc04418616ba278a7a6b940f8a7eea85db7
nullkal <nullkal@nil.nu>
2017-07-05 21:35:02 +0900

    Implement instance info acquirement

--- lib/mastodon_healthchecker/instance_info.rb
+++ lib/mastodon_healthchecker/instance_info.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'mastodon_healthchecker/hash_hosts'
 require 'nokogiri'
 require 'faraday'
@@ -8,32 +9,41 @@ require 'resolv-replace'
 
 module MastodonHealthchecker
   class InstanceInfo
-    def self.fetch(host, addresses)
-      default_resolver = Resolv::DefaultResolver
 
-      hash_hosts = HashHosts.new({ "#{host}" => addresses })
-      Resolv::DefaultResolver.replace_resolvers([hash_hosts, Resolv::DNS.new])
+    class << self
+      def fetch(host, addresses)
+        default_resolver = Resolv::DefaultResolver
 
-      result = nil
-      begin
+        hash_hosts = HashHosts.new({ "#{host}" => addresses })
+        Resolv::DefaultResolver.replace_resolvers([hash_hosts, Resolv::DNS.new])
+
+        result = nil
+        begin
+          info = InstanceInfo.new
+          info.parse_about(get(host, '/about'))
+          info.parse_about_more(get(host, '/about/more'))
+          info.parse_instance(get(host, '/api/v1/instance'))
+          result = info
+        rescue Faraday::ClientError
+        rescue Nokogiri::SyntaxError
+        rescue JSON::ParseError
+        end
+
+        Resolv::DefaultResolver.replace_resolvers(default_resolver)
+        result
+      end
+
+      private
+
+      def get(host, path)
         conn = Faraday.new("http://#{host}") do |faraday|
           faraday.use FaradayMiddleware::FollowRedirects
           faraday.response :encoding
           faraday.response :raise_error
           faraday.adapter :net_http
         end
-
-        info = InstanceInfo.new
-        info.parse_about(conn.get('/about'))
-        info.parse_about_more(conn.get('/about/more'))
-        info.parse_instance(conn.get('/api/v1/instance'))
-        result = info
-      rescue Faraday::ClientError
-      rescue Nokogiri::SyntaxError
+        conn.get(path)
       end
-
-      Resolv::DefaultResolver.replace_resolvers(default_resolver)
-      result
     end
 
     def initialize
@@ -45,22 +55,36 @@ module MastodonHealthchecker
 
       @description = nil
       @extended_description = nil
+      @email = nil
       @opened = nil
     end
 
     def parse_about(response)
-      # TODO: Implement here
+      doc = Nokogiri::HTML.parse(response.body)
+      @opened = doc.xpath("//*[@class='closed-registrations-message']").empty?
     end
 
     def parse_about_more(response)
-      # TODO: Implement here
+      doc = Nokogiri::HTML.parse(response.body)
+
+      info = doc.xpath("//*[@class='main']/*[@class='information-board']/*[@class='section']/strong")
+      if 3 <= info.length
+        @users = info[0].text.tr(',', '').to_i
+        @statuses = info[1].text.tr(',', '').to_i
+        @connections = info[2].text.tr(',', '').to_i
+      end
+
+      @extended_description = doc.xpath("//*[@class='main']/*[@class='panel'][2]").inner_html
     end
 
     def parse_instance(response)
-      # TODO: Implement here
+      instance = JSON.parse(response.body)
+      @version = instance['version']
+      @description = instance['description']
+      @email = instance['email']
     end
 
     attr_reader :version, :users, :statuses, :connections,
-                :description, :extended_description, :opened
+                :description, :extended_description, :email, :opened
   end
 end
